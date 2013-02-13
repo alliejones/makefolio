796dc5bc8bfa77945a9b915b57d1ce7dbec28a20
Allie Jones <allie.jones@gmail.com>
2013-02-13 15:17:54 -0500

    Make individual project pages with basic layout

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -1,18 +1,46 @@
 require_relative '../lib/makefolio'
+require 'fileutils'
+require 'RDiscount'
+require 'erb'
 
 describe Makefolio::Site do
   before(:each) do
-    @site = Makefolio::Site.new('./spec/_src/')
+    @site = Makefolio::Site.new('./spec/_src')
   end
 
   describe "when created" do
     it "should have a path" do
-      @site.path.to_s.should == './spec/_src/'
+      @site.path.to_s.should == './spec/_src'
+    end
+
+    it "should have a project path" do
+      @site.project_path.to_s.should == './spec/_src/projects'
+    end
+
+    it "should have a template path" do
+      @site.template_path.to_s.should == './spec/_src/templates'
     end
 
     it "should have a collection of projects" do
       names = @site.projects.collect { |p| p.name }
       names.should match_array([ 'one', 'two', 'three'])
+    end
+  end
+
+  describe "when generating" do
+    before(:each) do
+      FileUtils.rm_rf('./spec/dist/')
+      @site.generate
+    end
+
+    it "should create an html file for each project" do
+      dist = Pathname.new('./spec/dist')
+      files = dist.children.select { |c| c.file? }.collect { |c| c.relative_path_from(dist).to_s }
+      files.should match_array([ 'one.html', 'two.html', 'three.html' ])
+    end
+
+    after(:all) do
+      FileUtils.rm_rf('./spec/dist/')
     end
   end
 end
@@ -28,9 +56,29 @@ describe Makefolio::Project do
       @project.path.to_s.should == './spec/_src/projects/one'
     end
 
-    it "should have a desc property with the contents of <name>.md" do
-      desc = IO.read('./spec/_src/projects/one/one.md')
+    it "should have a desc property with the contents of <name>.md converted to HTML" do
+      desc = RDiscount.new(IO.read('./spec/_src/projects/one/one.md')).to_html
       @project.desc.should == desc
+    end
+  end
+
+  describe "when generated" do
+    before(:each) do
+      FileUtils.rm_rf('./spec/dist/')
+      @site.generate
+    end
+
+    it "should have an html file containing the layout and description" do
+      file_contents = IO.read('./spec/dist/one.html')
+
+      binding = ErbBinding.new({ :content => @project.desc }).get_binding
+      html = @site.erb_template.result(binding)
+
+      file_contents.should == html
+    end
+
+    after(:all) do
+      FileUtils.rm_rf('./spec/dist/')
     end
   end
 end
